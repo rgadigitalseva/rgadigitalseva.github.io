fda44af580769f5f51c0c4ca667503c7df2d1d4a
GAURAV KUMAR <rgadigitalseva@gmail.com>
2026-03-18 19:37:55 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,125 @@
+let data = [
+
+  // ===== MAIN SERVICES =====
+  {
+    name: "CSC ID Registration 2026",
+    link: "https://cscregister.csccloud.in/",
+    img: "images/logo.png"
+  },
+  {
+    name: "TEC Certificate Exam",
+    link: "https://www.cscentrepreneur.in/register",
+    img: "images/logo.png"
+  },
+  {
+    name: "Ration Card Bihar 2026",
+    link: "ration-card.html",
+    img: "images/logo.png"
+  },
+  {
+    name: "Bihar Post Matric Scholarship",
+    link: "bihar-post-matric-scholarship.html",
+    img: "images/banner.jpg"
+  },
+
+  // ===== LATEST UPDATE =====
+  {
+    name: "PM Awas Yojana Online Apply 2026",
+    link: "pm-awas-yojana.html",
+    img: "images/banner.jpg"
+  },
+  {
+    name: "Bihar Farmer ID Download",
+    link: "bihar-farmer-id.html",
+    img: "images/banner3.jpg"
+  },
+  {
+    name: "CTET Admit Card 2026",
+    link: "ctet-exam-city-admit-card.html",
+    img: "images/logo.png"
+  },
+  {
+    name: "PAN Aadhaar Link 2026",
+    link: "pan-aadhaar-link.html",
+    img: "images/logo.png"
+  },
+  {
+    name: "UP BEd Online Form 2026",
+    link: "up-bed-online-form-2026.html",
+    img: "images/logo.png"
+  },
+
+  // ===== JOBS =====
+  {
+    name: "India Post GDS Vacancy 2026",
+    link: "india-post-gds-vacancy-2026.html",
+    img: "images/logo.png"
+  },
+  {
+    name: "RRB Group D Vacancy 2026",
+    link: "railway-group-d-2025.html",
+    img: "images/logo.png"
+  },
+
+  // ===== ADMIT CARD =====
+  {
+    name: "SSC Admit Card 2026",
+    link: "ssc-admit.html",
+    img: "images/logo.png"
+  },
+  {
+    name: "Railway Admit Card",
+    link: "railway-admit.html",
+    img: "images/logo.png"
+  },
+  {
+    name: "UP Police Admit Card",
+    link: "up-police-admit.html",
+    img: "images/logo.png"
+  },
+
+  // ===== RESULTS =====
+  {
+    name: "Bihar Board 10th Result 2026",
+    link: "bseb-10th-result-2026.html",
+    img: "images/banner-bseb-10th-result-2026.jpeg"
+  },
+  {
+    name: "Bihar Board 10th Result 2026",
+    link: "bseb-12th-result-2026.html",
+    img: "images/banner-bseb-12th-result-2026.jpeg"
+  },
+  {
+    name: "UPSC Result",
+    link: "upsc-result.html",
+    img: "images/logo.png"
+  },
+  {
+    name: "Railway Result",
+    link: "railway-result.html",
+    img: "images/logo.png"
+  },
+
+  // ===== IMPORTANT LINKS =====
+  {
+    name: "Aadhar Download",
+    link: "aadhar.html",
+    img: "images/logo.png"
+  },
+  {
+    name: "Voter ID Status",
+    link: "voter.html",
+    img: "images/logo.png"
+  },
+  {
+    name: "PAN Card Apply",
+    link: "pan.html",
+    img: "images/logo.png"
+  },
+  {
+    name: "Driving License Apply",
+    link: "dl.html",
+    img: "images/logo.png"
+  }
+
+];
